7f0b42f7f591d0c69f3c70e8b1d3701b6dbdec8b
Nondas Ioannou <nevwalkalone13@gmail.com>
2021-10-26 01:11:33 +0300

    Comments added

--- 1st Assignment/src/pizza1.c
+++ 1st Assignment/src/pizza1.c
@@ -1,506 +1,326 @@
-#include "p3140059-p3180077-p3180193-pizza1.h"
+#include "pizza1.h"
 
 
 int main(int argc, char ** argv){
 
-	//check for number of arguments
-
+	// check for number of arguments
 	if (argc!=3 ){
 	
 		printf("Incorrect number of arguments, must be 2! \n");
-		
 		exit (-1);
-	
 	}
 	
-	//check for positive number
-	
+	// check for positive number
 	if(!isNumber(argv[1])  || !isNumber(argv[2])){
-		
+	
 		printf("Both arguments must be positive numbers!\n");
-		
 		exit (-1);
 	}
 	
-	//number of orders
-	int Ncust=atoi(argv[1]);
+	// number of orders
+	int Ncust = atoi(argv[1]);
 	
-	//table that will contain all times
-	//for the orders
+	// table that will contain all times for the orders
 	F_times = (long*)malloc((Ncust+1) * sizeof(long));
 	
-	//checking for memory leaks
+	// checking for memory leaks
 	memory_check(F_times);
-	
-	
-	
 	F_times[0] = 0;
 	
-	//seed
+	// seed
 	unsigned int seed=atoi(argv[2]);
 
-	
 	//table that will contain all threads
 	pthread_t* threads=(pthread_t*)malloc(Ncust*sizeof(pthread_t));
 	
-	//checking for memory leaks
+	// checking for memory leaks
 	memory_check(threads);
 	
+	// table with the orders ids
+	int* id = (int*)malloc(Ncust*sizeof(int));
 	
-	//table with the orders ids
-	int* id =(int*)malloc(Ncust*sizeof(int));
-	
-	//checking for memory leaks
+	// checking for memory leaks
 	memory_check(id);
 	
 	
-	//int variable for 
-	//checking pthread actions are ok
-
+	// int variable for checking  if pthread actions are ok
 	int rc;
 	
-
-	
-	//mutexes and conditions initializations
-	//and response code checking for each
-	
+	// mutexes and conditions initializations and response
+	// code checking for each
 	rc=pthread_mutex_init(&oven_lock,NULL);
-	
 	rc_check(rc);
-
 
 	rc=pthread_mutex_init(&cook_lock,NULL);
-	
 	rc_check(rc);
 
-
-
-	
 	rc=pthread_cond_init(&oven_cond,NULL);
-	
 	rc_check(rc);
-
-
 
 	rc=pthread_cond_init(&cook_cond,NULL);
-	
 	rc_check(rc);
 	
-
-
-
 	rc=pthread_mutex_init(&time_lock,NULL);
-	
 	rc_check(rc);
 	
-
-
 	rc=pthread_mutex_init(&screen_lock,NULL);
-	
 	rc_check(rc);
 
 	
-	//creating the threads
-	
+	// creating the threads	
 	for (int i=0; i<Ncust; i++){
 		
-		//ids and number of pizzas
+		// ids and number of pizzas
 		id[i]=i+1;
 		pizzas_ids x;
 		x.number_of_pizzas=rand_r(&seed) % Norderhigh + Norderlow;
 		x.id=id[i];
 
-		//changing the seed for
-		//each thread
-		//so it's more random
+		// changing the seed for each thread so it's
+		// more random
 		seed=seed+id[i];
 		
 		rc=pthread_create(&threads[i],NULL,order,(void*)&x);
-		
 		rc_check(rc);
 		
 		unsigned int y= rand_r(&seed) %(Torderhigh)+(Torderlow);
-		
-		//sleeping until next order 
-		sleep(y);
-		
+
+		// sleeping until next order 
+		sleep(y);	
 	}
 	
 	
-	//waiting for all threads
-	//to end their routine
-	
+	// waiting for all threads to end their routine
 	for (int i=0; i<Ncust; i++){
 		
 		rc=pthread_join(threads[i], NULL); 
-		
 		rc_check(rc);
 	}
-
-
-
-
 	
-	//Destroying all mutexes and conditions
-	
+	// Destroying all mutexes and conditions
 	rc=pthread_mutex_destroy(&oven_lock);
-	
 	rc_check(rc);
-
-
-
 
 	rc=pthread_mutex_destroy(&cook_lock);
-	
 	rc_check(rc);
-
-
-
 
 	rc=pthread_cond_destroy(&oven_cond);
-	
 	rc_check(rc);
-
-
-
 
 	rc=pthread_cond_destroy(&cook_cond);
-	
 	rc_check(rc);
-
-
-
 
 	rc=pthread_mutex_destroy(&time_lock);
-	
 	rc_check(rc);
-
-
 
 	rc=pthread_mutex_destroy(&screen_lock);
-	
 	rc_check(rc);
 
 
 	
-	//sum to calculate
-	//average time
-	
+	// sum to calculate average time
 	int sum = 0;
 
 	for (int i = 0;i<Ncust;i++) {
 
 		sum += F_times[i+1];		
-
-
-		}
+	}
 	
-	double avg=(double)sum/Ncust;
-	
-	
+	double avg = (double) sum/Ncust;	
 	time_t max = 1;	
 	
-	//calculating max time
-	
+	// calculating max time
 	for (int i = 1; i<=Ncust ; i++) {
 	
 		if(F_times[i] > max) { max = F_times[i]; }
-
 	}
 
-	//print messages for max and average time
+	// print messages for max and average time
 	
 	printf("\nAverage time: %.1f minutes \n", avg);
-	
 	printf("Maximum time: %ld   minutes\n", max);
 	
 	
-	//freeing up
-	//memory
-	
+	// freeing up memory
 	free(F_times);
 	free(threads);
 	free(id);
-
 }
 
-
-
-
-
-
-//routine for the threads
-//to follow
-
+// check pizza1.h 
 void* order(void * x){
 
-		
-	//using the struct we 
-	//have created to store
-	//the id and the number of pizzas
-	
+	// using the struct we have created to 
+	// to store the id and the number of pizzas
 	pizzas_ids* thread_arg;
 	thread_arg = (pizzas_ids *)x;
 	
 	
-	//initializing the variables
-	
+	// initializing the variables
 	int id = thread_arg->id;
 	int pizzas = thread_arg->number_of_pizzas;
-	
 	int rc;
 	
 	
-	//locking the time_lock mutex
-	
+	// locking the time_lock mutex
 	rc=pthread_mutex_lock(&time_lock);
-	
 	rc_check(rc);
 	
-	//storing the starting time of the order
+	// storing the starting time of the order
 	
 	clock_gettime(CLOCK_REALTIME , &start);
-	
 	F_times[id] = start.tv_sec;
 	
-
 	rc=pthread_mutex_unlock(&time_lock);	
-	
 	rc_check(rc);
-	
-	
-	
-	//locking the cook_lock mutex
+		
+	// locking the cook_lock mutex
 
 	rc=pthread_mutex_lock(&cook_lock);
-	
 	rc_check(rc);
 
-	//while there are no cooks available
-	//the thread waits
-	
+	// while there are no cooks available the thread
+	// waits
 	while(Ncook==0){
 		
 		rc=pthread_cond_wait(&cook_cond, &cook_lock);
-		
 		rc_check(rc);
-
-
 	}
 	
-	//there is a cook to handle the specific order now
-
+	// there is a cook to handle the specific order now
 	Ncook--;
 	
-	
-	//cook unlock
-
+	// cook unlock
 	rc=pthread_mutex_unlock(&cook_lock);
-	
 	rc_check(rc);
 
-	//pizzas preparation time
-
+	// pizzas preparation time
 	sleep(pizzas*Tprep);
 	
-	//now the order is ready to enter
-	//the oven, so we lock it with 
-	//the appropriate mutex and check if
-	//there are any available ovens
-
+	// now the order is ready to enter the oven so we
+	// so we lock it with the appropriate mutex and check
+	// if there are any available ovens
 	rc=pthread_mutex_lock(&oven_lock);
-
 	rc_check(rc);
 
 	
-	//while there are no ovens available
-	//the thread waits
-
+	// while there are no ovens available the thread
+	// waits
 	while(Noven==0){
 
 		rc=pthread_cond_wait(&oven_cond,&oven_lock);
-
 		rc_check(rc);
-
-
 	}
 	
-	//there is an oven to enter now
-
+	// there is an oven to enter now
 	Noven--;
 
-	
-	//oven unlock
-
+	// oven unlock
 	rc=pthread_mutex_unlock(&oven_lock);
-
 	rc_check(rc);
 
-	//bake time 
-
+	// bake time 
 	sleep(Tbake);
 	
-
-	//locking the oven_lock again
-	//to make changes now the order
-	//is done
-
+	// locking the oven_lock again to make changes, now
+	// the order is done
 	rc=pthread_mutex_lock(&oven_lock);
-
 	rc_check(rc);
 
-	//the oven that was busy with the specific order
-	//is now available
-
+	// the oven that was busy with the specific order is now
+	// available
 	Noven++;
 
-
-	//we signal the threads that are sleeping
-
+	// we signal the threads that are sleeping
 	rc=pthread_cond_signal(&oven_cond);
-
 	rc_check(rc);
 
-
-	//oven unlock
-
+	// oven unlock
 	rc=pthread_mutex_unlock(&oven_lock);
-
 	rc_check(rc);
 
-	//cook lock
-
+	// cook lock
 	rc=pthread_mutex_lock(&cook_lock);
-
 	rc_check(rc);
 	
-	//same logic as above
-
+	// same logic as above
 	Ncook++;
 
 
-	//we signal again the threads that are sleeping 
-
+	// we signal again the threads that are sleeping 
 	rc=pthread_cond_signal(&cook_cond);
-
 	rc_check(rc);
 
-	//cook unlock
-
+	// cook unlock
 	rc=pthread_mutex_unlock(&cook_lock);
-
 	rc_check(rc);
 
-	
-	//time_lock to calculate the time
-	//with gettime function
-
+	// time_lock to calculate the time with 
+	// gettime function
 	rc=pthread_mutex_lock(&time_lock);
-		
 	rc_check(rc);
 	
-	//calculating time and storing it
-	//in F_times table
-
+	// calculating time and storing it in F_times
+	// table
 	clock_gettime(CLOCK_REALTIME, &finish);
-
 	time_t finish_time = finish.tv_sec;
-
 	time_t f_time = finish_time - F_times[id];
-
 	F_times[id] = f_time;
 
-	//time unlock
-
+	// time unlock
 	rc=pthread_mutex_unlock(&time_lock);
-
 	rc_check(rc);
 
-
-	
-	//screen lock to print order's messages
-
+	// screen lock to print order's messages
 	rc=pthread_mutex_lock(&screen_lock);
-
 	rc_check(rc);
 
-	//order is finished, so we print the appropriate message
-	
+	// order is finished, so we print the appropriate message
 	printf("Order with id %d and %d pizzas was served successfully in %ld minutes \n", id,pizzas, F_times[id]);
-	
 
-	//screen unlock
-
+	// screen unlock
 	rc=pthread_mutex_unlock(&screen_lock);
-	
 	rc_check(rc);
 
-
-	//thread exits
-	
-	pthread_exit(NULL);
-	
-
+	// thread exits	
+	pthread_exit(NULL);	
 }
 
-
-//function for checking
-//memory leaks
-
+// check pizza1.h 
 void memory_check(void* pointer){
 
 	if (pointer==NULL){
 		
 		printf("No memory available!\n");
-
-		exit (-1);
-		
-
+		exit (-1);	
 	}
-
 }
 
-//function for checking
-//if all pthread actions
-//are ok
-
+// check pizza1.h 
 void rc_check(int rc){
 
-	if (rc != 0) {	
+	if (rc != 0) {
 
 		printf("ERROR: return code from the specific pthread action is %d\n", rc);
-
 		pthread_exit(&rc);
 	}
-
-
 }
 
-
-
-//function for checking
-//if the argument is a 
-//positive number
-
+// check pizza1.h 
 bool isNumber(char number[])
 {
-    
-    //checking for negative numbers
+    // checking for negative numbers
     if(number[0]=='-'){
-
 		return false;
     }
 	
 	for (int i=0; number[i] != 0; i++)
-    {
-       
+    {	
+		// if it is not a digit return false
         if (!isdigit(number[i])){
-
-            return false;
-		
-	}	
+            return false;	
+		}	
     }
-
     return true;
 }
 
--- 1st Assignment/src/pizza1.h
+++ 1st Assignment/src/pizza1.h
@@ -2,15 +2,14 @@
 #include <stdio.h>
 #include <pthread.h>
 
-//for bool types
+// for bool types
 #include <stdbool.h>
 
-//to avoid implicit
-//declaration warnings
+// to avoid implicit declaration warnings
 #include <unistd.h>
 #include <ctype.h>
 
-//constants
+// constants
 #define Torderlow 1
 #define Torderhigh 5
 #define Norderlow 1
@@ -18,63 +17,41 @@
 #define Tprep 1
 #define Tbake 10
 
-//initial number of
-//cooks and ovens
+// initial number of cooks and ovens
 int Ncook=2;
 int Noven=5;
 
-
-//variables initialization
-//for gettime function
+// variables initialization for gettime function
 struct timespec start,finish;
 long * F_times;
 
-
-
-//routine declaration
+// thread routine declaration
 void * order(void * x);
 
-
-
-//function declaration
-//for checking if an argument
-//is a positive number
+// function for checking if an argument is a 
+// positive number
 bool isNumber(char number[]);
 
-
-
-//function for checking 
-//pthread actions are ok
+// function for checking if pthread actions are ok
 void rc_check(int rc);
 
-
-
-//function for checking any possible memory leaks
+// function for checking any possible memory leaks
 void memory_check(void* pointer);
 
-
-
-//mutexes declarations
+// mutexes declarations
 pthread_mutex_t oven_lock;
 pthread_mutex_t cook_lock;
 pthread_mutex_t time_lock;
 pthread_mutex_t screen_lock;
 
-
-
-//conditions declarations
+// conditions declarations
 pthread_cond_t oven_cond; 
 pthread_cond_t cook_cond;
 
-
-
-//struct declaration
-//for the order arguments
+// struct declaration for the order arguments
 typedef struct order_args{
 	int number_of_pizzas;
 	int id;
 	//time_t f_time;
-
-
 
 } pizzas_ids;
